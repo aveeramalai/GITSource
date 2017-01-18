0d68c9eeece2fc21bfbb105f807524aefcc173a9
aveeramalai <andavarv@gmail.com>
2017-01-17 19:14:03 -0600

    add new project

---
+++ WebHooksDemo/WebHooksDemo/App_Start/WebHookConfig.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+using System.Web.Http;
+
+namespace WebHooksDemo
+{
+    public static class WebHookConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+
+			config.InitializeReceiveAzureAlertWebHooks();
+
+			config.InitializeReceiveGitHubWebHooks();
+
+        }
+    }
+}
---
+++ WebHooksDemo/WebHooksDemo/WebHookHandlers/AzureAlertWebHookHandler.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+
+
+using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHooksDemo.WebHookHandlers
+{
+    public class AzureAlertWebHookHandler : WebHookHandler
+    {
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+			// make sure we're only processing the intended type of hook
+			if("AzureAlert".Equals(receiver, System.StringComparison.CurrentCultureIgnoreCase))
+			{
+				// todo: replace this placeholder functionality with your own code
+				string action = context.Actions.First();
+				JObject incoming = context.GetDataOrDefault<JObject>();
+			}
+            
+            return Task.FromResult(true);
+        }
+    }
+}
